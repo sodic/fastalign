35f3369a81045a6b73d4a6825c585bd3a350317c
Filip Sodić <filip.sodic@fer.hr>
2018-04-13 19:11:14 +0200

    implement reference sequence indexing

--- src/FASTARead/FASTARead.cpp
+++ src/FASTARead/FASTARead.cpp
@@ -2,22 +2,21 @@
 // Created by filip on 03.04.18..
 //
 
-#include "FASTARead.h"
-
+#include <cstdint>
 #include "FASTARead.h"
 
 const char* FASTARead::get_name(){
-    return name_;
+    return name_.c_str();
 };
 
 const char* FASTARead::get_data(){
-    return data_;
+    return data_.c_str();
 };
 
 uint32_t FASTARead::get_data_length(){
-    return data_;
+    return static_cast<uint32_t>(data_.size());
 }
 
 uint32_t FASTARead::get_name_length() {
-    return name_;
+    return static_cast<uint32_t>(name_.size());
 }
--- src/FASTARead/FASTARead.h
+++ src/FASTARead/FASTARead.h
@@ -5,22 +5,23 @@
 #ifndef ZAVRSNI_FASTAREAD_H
 #define ZAVRSNI_FASTAREAD_H
 
+#include <string>
 
 /**
  * The class FASTARead encapsulates information about a read taken from a FASTA file.
  * The class with this format was required by the library bioparser (https://github.com/rvaser/bioparser) used for reading data from FASTA files.
  */
 class FASTARead {
-    uint64_t id_;
-    std::string name_;
-    std::string data_;
+
+    std::string name_{};
+
+    std::string data_{};
 
 public:
-    FASTARead(uint64_t id,
-              const char *name,
+    FASTARead(const char *name,
               uint32_t name_length,
               const char *data,
-              uint32_t data_length) : id_(id), name_(name, name_length), data_(data, data_length) {
+              uint32_t data_length) : name_(name, name_length), data_(data, data_length) {
     }
 
     const char*  get_data();
